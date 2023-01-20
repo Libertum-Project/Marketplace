67a4e049a49590271c008cfc0dc5cb12409d33e7
Agustin Panizza <agustin_panizza@hotmail.com>
2023-01-20 10:46:58 -0400

    Astar netowkr Metamask

--- LBM-Client/src/components/Dashboards/Deposit/Row.jsx
+++ LBM-Client/src/components/Dashboards/Deposit/Row.jsx
@@ -1,59 +1,52 @@
 import { useState } from "react";
 import "./Deposit.scss";
-import BTC from "./assets/BTC.svg"
-import LTC from "./assets/LTC.svg"
-import ETH from "./assets/ETH.svg"
+import BTC from "./assets/BTC.svg";
+import LTC from "./assets/LTC.svg";
+import ETH from "./assets/ETH.svg";
+export default function Row({ img, name, amount, value }) {
+  const [render, setRender] = useState(value);
+  const [active, setActive] = useState({
+    supply: false,
+    borrow: false,
+  });
 
-export default function Row({img,name, amount, value}) {
+  var urlImg;
 
-    const [render , setRender] = useState(value)
-    const [active, setActive] =useState({
-        supply: false,
-        borrow: false
-    });
+  if (img === "BTC") {
+    urlImg = BTC;
+  } else if (img === "LTC") {
+    urlImg = LTC;
+  } else if (img === "ETH") {
+    urlImg = ETH;
+  }
 
-   var urlImg
+  const handleClick = () => {
+    setRender(!render);
+  };
 
-    if(img === 'BTC') {
-        urlImg = BTC
-    }
-    else if(img ==='LTC') {
-        urlImg = LTC
-    }
-    else if(img==='ETH') {
-        urlImg = ETH
-    }
+  const handleButton = (e) => {
+    let changed = e.target.name;
+    e.preventDefault();
+    setActive({});
+  };
 
-    const handleClick = () => {
-        setRender(!render)
-    };
-
-    const handleButton = (e) => {
-        let changed = e.target.name
-        e.preventDefault()
-        setActive({
-           
-        })
-    };
-
-    return (
-            <div className="row" >
-                <div className="innerRow2" onClick={handleClick}>
-                    <img src={urlImg} />
-                    <p>{name}</p>
-                </div>
-                {render && <div className="innerRow3" onClick={handleClick}>
-                    <p>{amount}</p>
-                </div>}
-                {!render &&<div className="innerRow">                      
-                    <button onClick={handleButton}>
-                        Supply
-                    </button>
-                    <button onClick={handleButton}>
-                       Withdraw
-                    </button>
-                </div>}
-            </div>
-    )
-};
-
+  return (
+    <div className="row">
+      <div className="innerRow2" onClick={handleClick}>
+        <img src={urlImg} />
+        <p>{name}</p>
+      </div>
+      {render && (
+        <div className="innerRow3" onClick={handleClick}>
+          <p>{amount}</p>
+        </div>
+      )}
+      {!render && (
+        <div className="innerRow">
+          <button onClick={handleButton}>Supply</button>
+          <button onClick={handleButton}>Withdraw</button>
+        </div>
+      )}
+    </div>
+  );
+}
