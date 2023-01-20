dc1869be8dc1078dc824feffa9e2dd50d92e0357
Agustin Panizza <agustin_panizza@hotmail.com>
2023-01-20 10:57:59 -0400

    Astar network

--- LBM-Client/src/components/Dashboards/Dash/Dash.jsx
+++ LBM-Client/src/components/Dashboards/Dash/Dash.jsx
@@ -1,34 +1,33 @@
-import React from "react";
-import Slider from "../Slider/Slider";
-import VertSlider from "../VertSlider/VertSlider";
-import Mint from "../Mint/Mint";
-import UserInfo from "../UserInfo/UserInfo";
-import FinancialMarket from "../Deposit/Deposit";
-import "./Dash.scss";
-
-function Dash({setStateModal}) {
+function Dash({ setStateModal }) {
   return (
-
     <div className="general">
       <div className="dash">
-      <div className="columnOne">
-        <div className="account">
-          <h1 className="show">Dashboard</h1>
-          <UserInfo/>
+        <div className="columnOne">
+          <div className="account">
+            <h1 className="show">Dashboard</h1>
+            <UserInfo />
+          </div>
+          <div className="hidden">
+            <FinancialMarket />
+          </div>
         </div>
-        <div className="hidden">
-          <FinancialMarket/>
+        <div className="columnTwo">
+          <div className="hidden">
+            <Slider />
+          </div>
+          <div className="hidden">
+            <VertSlider setStateModal={setStateModal} />
+          </div>
+          <Mint />
         </div>
-      </div>
-      <div className="columnOne">
-        <div className="hidden">
-          <Slider />
+        <div className="markets">
+          <h2>Markets</h2>
+          <div className="buttons">
+            <button className="button">Supply</button>
+            <button className="button">Borrow</button>
+          </div>
+          <SupplyMarket />
         </div>
-        <div className="hidden">
-          <VertSlider setStateModal={setStateModal} />
-        </div>
-        <Mint />
-      </div>
       </div>
     </div>
   );
--- LBM-Client/src/components/Dashboards/UserInfo/UserInfo.jsx
+++ LBM-Client/src/components/Dashboards/UserInfo/UserInfo.jsx
@@ -21,8 +21,6 @@ export default function UserInfo() {
       setApy("7.5%");
     } else setApy("3.2%");
   };
-
-<<<<<<< HEAD
   return (
     <div className="containerUi">
       <h1 className="title">My account</h1>
@@ -46,64 +44,6 @@ export default function UserInfo() {
         <div>
           <h3>Daily earnings</h3>
           <h4>$0.00</h4>
-=======
-
-    return (
-        <div className="containerUi">
-            <div className="headerUI">
-                <h1 className="title">My account</h1>
-                <div className="toggle">APY with LBM</div>
-                <Switch 
-                    checked={toggle} 
-                    onChange={handleToggleChange}
-                    className="switch" 
-                    onColor="#F7931A"
-                    uncheckedIcon={false}
-                    checkedIcon={false}
-                    width={51}
-                    height={31}
-                    handleDiameter={27}
-                />
-            </div>    
-                <div className="netApy" style={{left: '27px',top: '80px'}}>Net APY</div>
-                <div className="apy">{apy}</div>
-                <div className="grid">
-                    <div className="gridRow">
-                        <h3>Daily earnings</h3>
-                        <h4>$0.00</h4>
-                    </div>
-                    <div className="gridRow">
-                        <h3>Supply balance</h3>
-                        <h4>$0.00</h4>
-                    </div>
-                    <div className="gridRow">
-                        <h3>Borrow balance</h3>
-                        <h4>$0.00</h4>
-                    </div>
-                </div>
-                <div className="fillBar">
-                    <div className="labelsFiller">
-                        <div className="borrowLimit" style={{width: '135px', height: '16px'}}>
-                           <h3>Borrow limit used: </h3> 
-                           <h4>{' 0%'}</h4> 
-                        </div>
-                        <div className="borrowLimit" style={{width: '76px', height: '16px'}}>
-                           <h3>Limit:  </h3> 
-                           <h4>{' $0.00'}</h4> 
-                        </div>      
-                    </div>
-                    <div className='fillerBar'>
-                        <ProgressBar bgcolor={"#4CAF50"} completed={bar.completed} limit={bar.limit}/>
-                    </div>
-                    
-                </div>
-                <div className="safeLimit">
-                    <img src={Shield} className="shield" />
-                    <h3>Your safe limit:</h3>
-                    <h4>{' $0.00'}</h4>
-                </div>
-                
->>>>>>> 6eb8b60 (User Info-responsive)
         </div>
         <div>
           <h3>Supply balance</h3>
