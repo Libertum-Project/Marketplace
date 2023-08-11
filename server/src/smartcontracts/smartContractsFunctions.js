8afcc83731570d1b347a46a0af186317ec79b6de
madahva <galarzaguillermo.ggm@gmail.com>
2023-08-11 18:30:59 -0300

    Feat (Server): Set property Status

--- server/src/smartcontracts/smartContractsFunctions.js
+++ server/src/smartcontracts/smartContractsFunctions.js
@@ -5,8 +5,23 @@ const {
   createPassiveIncomeProperty,
 } = require("./contracts/propertyFactoryAndBank/createPassiveIncomeProperty");
 
-const { mintCapitalRepaymentToken } = require('./contracts/capitalRepaymentProperty/mintToken');
-const { mintPassiveIncomeToken } = require('./contracts/passiveIncomeProperty/mintToken')
+const {
+  mintCapitalRepaymentToken,
+} = require("./contracts/capitalRepaymentProperty/mintToken");
+const {
+  mintPassiveIncomeToken,
+} = require("./contracts/passiveIncomeProperty/mintToken");
+
+const {
+  setActiveCapitalRepayment,
+  setInactiveCapitalRepayment,
+} = require("./contracts/propertyFactoryAndBank/setCapitalRepaymentStatus");
+
+const {
+    setActivePassiveIncome,
+    setInactivePassiveIncome,
+} = require("./contracts/propertyFactoryAndBank/setPassiveIncomeStatus");
+
 
 async function createSmartContractProperty(propertyData, propertyID) {
   if (propertyData.financialData.Investment_type === "capitalRepayment") {
@@ -16,12 +31,29 @@ async function createSmartContractProperty(propertyData, propertyID) {
   }
 }
 
-async function mintToken(userPrivateKey, propertyAddress, quantity, propertyType) {
-  if (propertyType === 'capitalRepayment') {
+async function mintToken(
+  userPrivateKey,
+  propertyAddress,
+  quantity,
+  propertyType
+) {
+  if (propertyType === "capitalRepayment") {
     await mintCapitalRepaymentToken(userPrivateKey, propertyAddress, quantity);
-  } else if (propertyType === 'passiveIncome') {
+  } else if (propertyType === "passiveIncome") {
     await mintPassiveIncomeToken(userPrivateKey, propertyAddress, quantity);
   }
 }
 
-module.exports = { createSmartContractProperty, mintToken };
+async function setPropertyStatusSmartContracts(propertyAddress, propertyType, status) {
+  if (propertyType === "capitalRepayment" && status === true) {
+    await setActiveCapitalRepayment(propertyAddress);
+  } else if (propertyType === "capitalRepayment" && status === false) {
+    await setInactiveCapitalRepayment(propertyAddress);
+  } else if (propertyType === "passiveIncome" && status === true) {
+    await setActivePassiveIncome(propertyAddress);
+  } else if (propertyType === "passiveIncome" && status === false) {
+    await setInactivePassiveIncome(propertyAddress);
+  }
+}
+
+module.exports = { createSmartContractProperty, mintToken, setPropertyStatusSmartContracts };
