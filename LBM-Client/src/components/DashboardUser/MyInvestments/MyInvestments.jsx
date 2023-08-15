8702cf2b924c08f2a0d5b16ab959ce30b9dd91c8
nikkiburgos <burgosveganicole@gmail.com>
2023-08-14 21:56:41 -0300

    investments - graphics

--- LBM-Client/src/components/DashboardUser/MyInvestments/MyInvestments.jsx
+++ LBM-Client/src/components/DashboardUser/MyInvestments/MyInvestments.jsx
@@ -1,5 +1,21 @@
-const MyInvestments = () => {
+import Frame3 from "./Frame3/Frame3";
+import Table from "./UsersTable/Table"
+import css from "./MyInvestments.module.scss"
 
+const MyInvestments = ({name, id, transactions, investments}) => {
+
+    return(
+        <div>
+        <div className={css.container}>
+            <div className={css.frame}>
+                <Frame3 />
+            </div>
+            <div className="">
+                <Table />
+            </div>
+                </div>
+        </div>
+    )
 };
 
 export default MyInvestments;
