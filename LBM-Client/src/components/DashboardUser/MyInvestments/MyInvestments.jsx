6f5fe151bbccfd12859a1a536e129f407ea52477
nikkiburgos <burgosveganicole@gmail.com>
2023-08-17 16:06:06 -0300

    no investments alert

--- LBM-Client/src/components/DashboardUser/MyInvestments/MyInvestments.jsx
+++ LBM-Client/src/components/DashboardUser/MyInvestments/MyInvestments.jsx
@@ -1,28 +1,41 @@
 import Frame3 from "./Frame3/Frame3";
 import css from "./MyInvestments.module.scss"
 import Investments from "./UsersTable/Table";
-
-const MyInvestments = ({name, id, transactions, investments}) => {
-
-
-    console.log("user name " + name) 
-    
-    return(
-        <div>
-           
-        <div >
-            <div >
-                <Frame3 />
+import { Link } from "react-router-dom";
+const MyInvestments = ({ name, id, transactions, investments }) => {
+    console.log("user name " + name);
+  
+    if (investments.length === 0) {
+      return (
+        <div className={css.container}>
+          <div>
+            <div className={css.alertNoInvestment}>
+              <p>You don't have any investments yet.</p>
+              <p> Enter our marketplace and start investing!</p> 
+              <Link to="/marketplace">
+              <button> Marketplace! </button>
+              </Link>             
             </div>
-            <div className="">
-                <Investments 
-                investments = { investments }
-                transactions = { transactions }
-                />
-            </div>
-                </div>
+          </div>
         </div>
-    )
-};
-
-export default MyInvestments;
+      );
+    }
+  
+    return (
+      <div>
+        <div>
+          <div>
+            <Frame3 />
+          </div>
+          <div className="">
+            <Investments 
+            investments={investments} 
+            transactions={transactions} />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default MyInvestments;
+  
--- LBM-Client/src/components/DashboardUser/MyProperties/MyProperties.jsx
+++ LBM-Client/src/components/DashboardUser/MyProperties/MyProperties.jsx
@@ -5,19 +5,18 @@ const MyProperties = ({name, id, transactions, saved}) => {
   return(
     <>
 
-      <div>
-        <p>Hi, {name}! Here you can see your properties!</p>
-      </div>
+
     
       <div>
         <Frame3 />
       </div>
 
       <div>
-        <Properties />
+        <Properties 
+        
+        />
       </div>
-      <p>{transactions}</p>
-    
+          
     </>
 )
 };
