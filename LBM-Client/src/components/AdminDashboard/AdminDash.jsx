f9fded7ece59a7eecf897266cd9ca680a3a98f89
nikkiburgos <burgosveganicole@gmail.com>
2023-08-21 09:09:35 -0300

    buttton to marketplace

--- LBM-Client/src/components/AdminDashboard/AdminDash.jsx
+++ LBM-Client/src/components/AdminDashboard/AdminDash.jsx
@@ -1,9 +1,9 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   fetchAllProperties,
-//   setPropertyStatus,
-// } from "../../../redux/features/propertySlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchAllProperties,
+  setPropertyStatus,
+} from "../../../redux/features/propertySlice";
 import Frame1 from "./Frame1/Frame1";
 import Frame2 from "./Frame2/Frame2";
 import Frame3 from "./Frame3/Frame3";
@@ -11,28 +11,28 @@ import Admins from "./TableAdmins/TableAdmins";
 import styles from "./adminDash.module.css";
 
 const AdminDash = () => {
-  // const dispatch = useDispatch();
-  // const allProperties = useSelector((state) => state.property.allProperies);
+  const dispatch = useDispatch();
+  const allProperties = useSelector((state) => state.property.allProperies);
 
-  // useEffect(() => {
-  //   dispatch(fetchAllProperties());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAllProperties());
+  }, [dispatch]);
 
-  // const handleActive = async (propertyId, isActive) => {
-  //   await dispatch(setPropertyStatus({ propertyId, isActive }));
-  //   dispatch(fetchAllProperties());
-  // };
+  const handleActive = async (propertyId, isActive) => {
+    await dispatch(setPropertyStatus({ propertyId, isActive }));
+    dispatch(fetchAllProperties());
+  };
 
-  // const handlePause = async (propertyId, isActive) => {
-  //   await dispatch(setPropertyStatus({ propertyId, isActive }));
-  //   dispatch(fetchAllProperties());
-  // };
-  // console.log(allProperties.length > 0)
+  const handlePause = async (propertyId, isActive) => {
+    await dispatch(setPropertyStatus({ propertyId, isActive }));
+    dispatch(fetchAllProperties());
+  };
+  console.log(allProperties.length > 0)
 
   return (
     <div>
       <Frame1 />
-      {/* {allProperties.length > 0 && (
+      {allProperties.length > 0 && (
         <section className={styles.properties_container}>
           {allProperties.map((property) => (
             <div key={property.ID_Property} className={styles.property}>
@@ -64,7 +64,7 @@ const AdminDash = () => {
             </div>
           ))}
         </section>
-      )} */}
+      )}
       <Frame2 />
       <Frame3 />
       <Admins />
