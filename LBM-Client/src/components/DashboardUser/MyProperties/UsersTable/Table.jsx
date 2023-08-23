eeb67219c0057dd3f902ce709dfa9a1aefaf3ff1
nikkiburgos <burgosveganicole@gmail.com>
2023-08-23 19:47:12 -0300

    withdraw button

--- LBM-Client/src/components/DashboardUser/MyProperties/UsersTable/Table.jsx
+++ LBM-Client/src/components/DashboardUser/MyProperties/UsersTable/Table.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import DataTable from 'react-data-table-component';   
 import css from './TableUsers.module.scss';
 import { Link } from 'react-router-dom';
+import { withdrawFunds } from '../../../../../redux/features/propertySlice';
 
  
 
@@ -30,34 +32,95 @@ const columns = [
         name: "Property Value",
         selector: row => row.value
     },
+
     {
         name: "Status",
-        selector: row => row.status
+        selector: row => row.propertystatus
     },
     {
         name: 'Transaction History',
         selector: row => row.history,
         cell: row => (
-            <Link to={`http:/localhost:3001/${row.propertyID}`}>
-                <button>
-                {row.claim}
+            <Link to="/comingsoon">
+                <button
+                style={{
+                    backgroundColor: "gray", //color de fondo
+                    borderRadius: "5px", //borde redondeado
+                    color: "white", // color del texto
+                    padding: "8px", //espaciado interno            
+                  }}
+                  >
+                {row.history}
                 </button>
             </Link>
-           ) }]
+           ) 
+    },
+    {
+        name: "Withdraw",
+        selector: row => row.withdrawFunds,
+        cell: row => (
+            <Link to={`http:/localhost:3001/${row.propertyID}`}>
+        <button
+          style={{
+            backgroundColor: "gray", //color de fondo
+            borderRadius: "5px", //borde redondeado
+            color: "white", // color del texto
+            padding: "8px", //espaciado interno            
+          }}
+        >
+          {row.withdrawFunds}
+        </button>
+      </Link>
+        )
+    },
+]
 
 
 const Properties = ({transactions, publishedProperties}) => {   
 
+    
+    const dispatch = useDispatch();
+    const claimError = useSelector((state) =>state.property.error)
+    const [error, setError] = useState(claimError);
+
+    const handleClickWithdraw = (event, address, type) => {
+
+        const propertyType = type;
+        const propertyAddress = address;
+
+    event.preventDefault();
+    dispatch(
+        withdrawFunds({propertyAddress, propertyType})
+        .then(()=>{
+        console.log(error)
+        })
+    )
+    }
+
+
     const properties = publishedProperties.map((property) => ({
         id: property.ID_Property,
-        propertyID: `#${property.ID_Property}`,
+        propertyID: property.ID_Property,
         address: property.Feature.Address,
         type: property.Feature.Type,
         tokensavailable: property.Financial.Number_of_tokens_available,
-        // tokenssold: publishedProperties,
+        // tokenssold: publishedProperties,        
         value: property.Financial.Market_value_of_the_property,
         propertystatus: property.Feature.Occupancy_Status,
-        history: "fda",
+        history: "History",
+        withdrawFunds: (
+            <button
+                onClick={(event)=>
+                handleClickWithdraw(
+                    event,
+                    property.Feature.Address,
+                    property.Feature,Type
+                )}
+            >
+                Withdraw
+            </button>
+        )
+
 
       }));
 
