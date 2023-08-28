776770c9de78ac6394c7e70305a909e4779dbebd
nikkiburgos <burgosveganicole@gmail.com>
2023-08-28 13:57:06 -0300

    howit works

--- LBM-Client/src/components/Landing/Services/Services.jsx
+++ LBM-Client/src/components/Landing/Services/Services.jsx
@@ -1,81 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Slide } from "react-awesome-reveal";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import mkt from "./assets/mkt.svg";
-import launchpad from "./assets/launchpad.svg";
-import loans from "./assets/loans.svg";
-import "./Services.scss";
+import styles from './services.module.scss';
+import loginImage from './assets/login.svg'
+import marketplaceImage from './assets/marketplace.svg';
+import graphicsImage from './assets/graphics.svg'
 
-export default function Services() {
-  const { loginWithRedirect } = useAuth0();
+const servicesData = [
+  {
+    nombre: 'Join The Club',
+    image: loginImage,
+    description: 'Sign up to the platform and pick an opportunity that suits you'
+  },
+ {
+    nombre: 'Earn Passive income',
+    image: graphicsImage,
+    description: 'Purchase a digital share and enjoy the “Libertum” a second income provides.'
+ },
+ {
+    nombre: 'Build a Future',
+    image: marketplaceImage,
+    description: 'You decide if you want to resell your property or invest in a diversified property portfolio'
+  }
+];
 
-  const handleLogin = () => {
-    const redirectUri = `${window.location.origin}/marketplace/`;
-    loginWithRedirect({
-      redirectUri: redirectUri,
-    });
-  };
+const Services = () => {
+  const [activeItem, setActiveItem] = useState(0);
 
   return (
-    <main className="services_items">
-      <section className="services_section">
-        <Slide direction={"down"} triggerOnce={"true"}>
-          <div className="services_text">
-            <h2>Libertum Project Proposition</h2>
-            <p>
-              Libertum Project services are divided into 3 categories and were
-              developed in 3 different stages. In the first stage we developed
-              the DeFi 1.0 loans component with the functionality of minting
-              stablecoins, in the second stage we finalized the real estate
-              investment marketplace which provides for the tokenization of
-              property, and in the last stage we created an environment where we
-              encourage other projects to take part in offering loan services
-              backed by real estate property collateral
-            </p>
-          </div>
+    <div className={styles.section}>
+      <Slide direction={"down"} triggerOnce={false}>
+        <h1>How it works?</h1>
+      </Slide>
+      <div className={styles.topBar}>
+        <Slide direction={"up"} triggerOnce={false}>
+          {servicesData.map((service, index) => (
+            <div
+              key={index}
+              className={`${styles.item} ${activeItem === index ? styles.active : ''}`}
+              onClick={() => setActiveItem(index)}
+            >
+              <h2>{`${index + 1 < 10 ? '0' : ''}${index + 1} - ${service.nombre}`}</h2>
+            </div>
+          ))}
         </Slide>
-        <div className="services_quad">
-          <Slide direction={"left"} triggerOnce={"true"}>
-            <div className="services-quad_item" onClick={handleLogin}>
-              <img src={loans} />
-              <h3>Decentralized Finance Loans</h3>
-              <p>
-                The ability to passively earn interest through DeFi 2.0 loans.
-                Our proposition broadens the horizon of credit markets,
-                connecting the real estate, and secondary market with Defi
-                solutions
-              </p>
-              <div></div>
-            </div>
-          </Slide>
-          <Slide direction={"up"} triggerOnce={"true"}>
-            <div className="services-quad_item" onClick={handleLogin}>
-              <img src={mkt} />
-              <h3>Real Estate Marketplace</h3>
-              <p>
-                One of the first real estate marketplaces where you can buy,
-                sell, trade and invest in crypto or fiat-currency backed by real
-                estate collateral
-              </p>
-              <div></div>
-            </div>
-          </Slide>
-          <Slide direction={"right"} triggerOnce={"true"}>
-            <div className="services-quad_item" onClick={handleLogin}>
-              <img src={launchpad} />
-              <h3>Crypto Currency Launchpad</h3>
-              <p>
-                Libertum Project invites real estate owners, syndicates,
-                individuals and startups that want a solid experience in the
-                DeFi world are looking for liquidity
-              </p>
-              <div></div>
-              <br />
-            </div>
+      </div>
+
+      <div className={styles.content}>
+        <Slide direction={"left"} triggerOnce={false}>
+          <img
+            src={servicesData[activeItem].image}
+            alt={`Imagen ${activeItem + 1}`}
+          />
+        </Slide>
+        <div className={styles.overlay}>
+          <Slide direction={"right"} triggerOnce={false}>
+            <p>{servicesData[activeItem].description}</p>
           </Slide>
         </div>
-      </section>
-    </main>
+      </div>
+    </div>
   );
-}
+};
+
+export default Services;
