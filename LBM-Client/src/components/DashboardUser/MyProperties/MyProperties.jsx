25196bc9d021f8c76ee91fb637f08b904592fadd
nikkiburgos <burgosveganicole@gmail.com>
2023-09-05 11:29:43 -0300

    properties tab

--- LBM-Client/src/components/DashboardUser/MyProperties/MyProperties.jsx
+++ LBM-Client/src/components/DashboardUser/MyProperties/MyProperties.jsx
@@ -1,47 +1,39 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import Frame3 from "./Frame3/Frame3";
 import Properties from "./UsersTable/Table";
 import css from "./MyProperties.module.scss";
 import { Link } from "react-router-dom";
-import { withdrawFunds } from "../../../../redux/features/propertySlice";
 
 const MyProperties = ({name, transactions, investments, publishedProperties, email}) => {
+  return (
+    <div className={css.propcontainer}>
 
-
-  console.log(publishedProperties)
-  
-  if (publishedProperties.length === 0) {
-    return (
-      <div className={css.container}>
-          <div>
+    {publishedProperties.length === 0 ? (
             <div className={css.alertNoInvestment}>
-              <p>You don't have properties published yet.</p>
-              <p> Click the link below and list your first property!</p> 
-              <Link to="https://docs.google.com/forms/d/e/1FAIpQLSd8HkLol829WO2hii1aem2H1_VNXWY6-1J_kqQAQclMPwo2MA/viewform" target="_blank">
-              <button> List </button>
-              </Link>             
-            </div>
-          </div>
+              <p>You don't have properties published yet.  Click the link below and list your first property!</p>
+
+              <Link
+                to="https://docs.google.com/forms/d/e/1FAIpQLSd8HkLol829WO2hii1aem2H1_VNXWY6-1J_kqQAQclMPwo2MA/viewform"
+                target="_blank"
+              >
+                <button> List </button>
+              </Link>
+            </div> 
+      ) : null}
+
+        <div>
+          <Frame3 />
         </div>
-    )
-  }
-  return(
-    <>    
-      <div>
-        <Frame3 />
-      </div>
-
-      <div>
-        <Properties 
-        investments={investments} 
-        transactions={transactions}
-        publishedProperties={publishedProperties}
-        />
-      </div>
-
-    </>
-)
-};
+  
+        <div>
+          <Properties
+            investments={investments}
+            transactions={transactions}
+            publishedProperties={publishedProperties}
+          />
+        </div>
+      
+    </div>
+  );
+      }
 
 export default MyProperties;
