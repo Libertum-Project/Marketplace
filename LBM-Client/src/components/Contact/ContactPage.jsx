d43a11f5681ab59340d8940956ca9abd161ebe3d
nikkiburgos <burgosveganicole@gmail.com>
2023-09-19 18:08:56 -0300

    contact form connected

--- LBM-Client/src/components/Contact/ContactPage.jsx
+++ LBM-Client/src/components/Contact/ContactPage.jsx
@@ -1,38 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef  } from 'react';
+import { useNavigate } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 import './ContactPage.scss';
-import { motion } from "framer-motion";
-import { Bounce, Flip } from "react-awesome-reveal";
-import { FaHourglassEnd } from "react-icons/fa";
-import { BsCheckAll } from "react-icons/bs";
 
 const ContactPage = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    message: '',
-  });
-  const [success, setSuccess] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const form = useRef()
+  const navigate = useNavigate(); 
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleFormSubmit = async (e) => {
-    setLoading(true);
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    // Realiza la lógica de envío del formulario aquí
-    const formElem = document.querySelector("form");
-    const formData = new FormData(formElem);
-    
-    // Simula un tiempo de carga antes de establecer el éxito
-    setTimeout(() => {
-      setSuccess(true);
-      setLoading(false);
-    }, 500);
+    emailjs
+    .sendForm('service_o8ckx7h', 'template_r8p0gnq', form.current, 'K3xhSEXO5y1bVyrXc')
+    .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+          navigate('/support');
+      }, (error) => {
+          console.log(error.text);
+          alert("Your message could not be sent. Please try again or email us at hello@libertum.io.")
+      });
   };
 
   return (
@@ -42,38 +29,29 @@ const ContactPage = () => {
           <h2>GET IN TOUCH</h2>
           <p>Send us your consult. We will reply you back as soon as possible</p>
         </div>
-        <div className="contact-form-content">
-          {!success ? (
-            !loading ? (
-              <form onSubmit={handleFormSubmit} action="/tu-ruta-de-envío" method="POST" autoComplete="off">
+        <div className="contact-form-content">    
+              <form ref={form} onSubmit={sendEmail}>
                 <div className="form-group">
                   <input
                     type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleChange}
+                    name="user_name"
                     placeholder="Name"
                     required
-                    autoComplete="off"
                   />
                 </div>
-                <div className="form-group">
+                {/* <div className="form-group">
                   <input
                     type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleChange}
+                    name="user_email"
                     placeholder="Surname"
                     required
                     autoComplete="off"
                   />
-                </div>
+                </div> */}
                 <div className="form-group">
                   <input
                     type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
+                    name="user_email"
                     placeholder="E-mail"
                     required
                     autoComplete="off"
@@ -82,27 +60,12 @@ const ContactPage = () => {
                 <div className="form-group">
                   <textarea
                     name="message"
-                    value={formData.message}
-                    onChange={handleChange}
                     placeholder="Write here your message"
                     required
                   ></textarea>
                 </div>
-                <button type="submit" className='buttonSubmitForm'>Send</button>
-              </form>
-            ) : (
-              <Flip style={{ marginTop: "0px" }}>
-                <FaHourglassEnd size={28} color="gray" />
-              </Flip>
-            )
-          ) : (
-            <Bounce style={{ marginTop: "10px" }}>
-              <BsCheckAll size={30} color="#26c502" />
-              <h3 style={{ color: "#26c502", margin: "0px" }}>
-                ¡Thanks for subscribe!
-              </h3>
-            </Bounce>
-          )}
+                <button type="submit" value="Send" className='buttonSubmitForm'>Send</button>
+              </form>            
         </div>
       </div>
     </div>
