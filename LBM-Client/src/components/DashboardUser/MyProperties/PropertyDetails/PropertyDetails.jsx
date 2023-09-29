954c479da39f3f5a44275bf7aa26147d296174d6
nikkiburgos <burgosveganicole@gmail.com>
2023-09-29 10:38:35 -0300

    fixed photo in properry details editor

--- LBM-Client/src/components/DashboardUser/MyProperties/PropertyDetails/PropertyDetails.jsx
+++ LBM-Client/src/components/DashboardUser/MyProperties/PropertyDetails/PropertyDetails.jsx
@@ -74,7 +74,7 @@ const PropertyDetails = ({ property, closeModal }) => {
         <div className="header">
           {/* <h2>Property #{property.id}</h2> */}
           <div className="property-info">
-          <img src="{feature.Link_Image[0]}" alt="" />
+            <img src={feature.Link_Image[0]} alt="" />
 
             <div>
               <h3>{property.address}</h3>
