fb3d27c8c3b3d2da5818a0936c130b1943d5ad68
nikkiburgos <burgosveganicole@gmail.com>
2023-10-27 13:37:40 -0300

    added buy libertum to mobile navbar

--- LBM-Client/src/components/Landing/NavBar/NavbarMarket/PopUp/PopUpUser.jsx
+++ LBM-Client/src/components/Landing/NavBar/NavbarMarket/PopUp/PopUpUser.jsx
@@ -179,6 +179,7 @@ function PopUpUser({ setActiveMenu }) {
                 <a href="/">Home</a>
                 <a href="/about">About us</a>
                 <a href="/support">Support</a>
+                <a href="/ico">Buy Libertum</a>
                 <div
                   className={
                     active === "language"
