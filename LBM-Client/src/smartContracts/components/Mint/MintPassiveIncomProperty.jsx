10dd64daf1431e3e1bc9736ec0a7459c91b4d997
madahva <galarzaguillermo.ggm@gmail.com>
2023-10-30 09:01:24 -0300

    Fix (MintToken): Rectify Faucet calculation and add informative error messages

--- LBM-Client/src/smartContracts/components/Mint/MintPassiveIncomProperty.jsx
+++ LBM-Client/src/smartContracts/components/Mint/MintPassiveIncomProperty.jsx
@@ -22,12 +22,15 @@ function MintPassiveIncomeProperty({
   const [showPendingMessage, setShowPendingMessage] = useState(false);
   const [showFailMessage, setShowFailMessage] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [polyScanURL, setPolyScanURL] = useState('');
   const dispatch = useDispatch();
   const isConnected = useSelector(selectIsConnected);
   const usdtTokenAddress = '0x43A8768b6F9cA89D5436413609150c6FB087a29E';
 
   const handleBuyTokenBtn = async (event) => {
     event.preventDefault();
+    let transactionHash;
     try {
       setIsLoading(true);
       if (window.ethereum) {
@@ -50,9 +53,15 @@ function MintPassiveIncomeProperty({
 
         const tokenPrice = await passiveIncomePropertyContract.pricePerToken();
         const price = BigInt(quantity) * BigInt(tokenPrice) * BigInt(10 ** 6);
+
+        const amountInUSDT = price;
+        const decimals = 6;
+        const amountInBasicUnits =
+          amountInUSDT * BigInt(Math.pow(10, decimals));
+
         const faucetTransaction = await usdtTokenContract
           .connect(signer)
-          .faucet(999_999_100_000_000);
+          .faucet(amountInBasicUnits);
         faucetTransaction.wait();
 
         const approveTransaction = await usdtTokenContract
@@ -66,12 +75,11 @@ function MintPassiveIncomeProperty({
             gasLimit: 2000000,
           }
         );
+        transactionHash = mintTransaction.hash;
         setShowPendingMessage(true);
         const receipt = await mintTransaction.wait();
 
         if (receipt.status === 1) {
-          console.log('Transaction was successful');
-
           const pricePerToken = Number(tokenPrice);
 
           dispatch(
@@ -92,6 +100,20 @@ function MintPassiveIncomeProperty({
         console.log(receipt);
       } else alert('Metamask not found.');
     } catch (error) {
+      if (error.message.includes('user rejected transaction')) {
+        setErrorMessage(
+          "It looks like you rejected this transaction. Don't miss out on the opportunity to earn passive income!"
+        );
+      }
+
+      if (transactionHash) {
+        setPolyScanURL(`https://mumbai.polygonscan.com/tx/${transactionHash}`);
+        setErrorMessage('An error occurred while processing your request.');
+      }
+
+      if (errorMessage === '')
+        setErrorMessage('Oops! Something went wrong. Please try again later.');
+
       console.error(error);
       setIsLoading(false);
       setShowPendingMessage(false);
@@ -108,7 +130,8 @@ function MintPassiveIncomeProperty({
       {showFailMessage ? (
         <FailMessage
           setShowFailMessage={setShowFailMessage}
-          message= "It looks like you rejected this transaction. Don't miss out on the opportunity to earn passive income!"
+          message={errorMessage}
+          url={polyScanURL}
         />
       ) : null}
       {showSuccessMessage ? (
