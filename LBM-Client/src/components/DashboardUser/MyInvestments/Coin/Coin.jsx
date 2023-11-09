d51aa1acb2e4c28da798a88632053cb008adc3f0
nikkiburgos <burgosveganicole@gmail.com>
2023-11-09 18:34:47 -0300

    handle to buy button

--- LBM-Client/src/components/DashboardUser/MyInvestments/Coin/Coin.jsx
+++ LBM-Client/src/components/DashboardUser/MyInvestments/Coin/Coin.jsx
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react";
+import { getUserWhitelistAllocation } from "../../../../smartContracts/pLBM/getUserWhitelistAllocation";
+import { getUserBalance } from "../../../../smartContracts/pLBM/getUserBalance";
 import css from "./Coin.module.scss"; 
 import DataTable from "react-data-table-component";
+import { useNavigate } from "react-router-dom";
 
 const columns = [
   {
@@ -27,31 +31,50 @@ const columns = [
 
 const Coin = () => {
   const data = [0,];
-  const tokensPurchased = 21;
-  const whitelistAllocation = 150;
+  // const tokensPurchased = 21;
+  // const whitelistAllocation = 150;
+  const [userBalance, setUserBalance] = useState(null);
+
+  useEffect(() => {
+    const fetchUserBalance = async () => {
+      try {
+        const balance = await getUserBalance();
+        setUserBalance(balance);
+      } catch (error) {
+        console.error('Error fetching user balance:', error);
+      }
+    };
+    fetchUserBalance();
+  }, []); 
+console.log(userBalance)
+
+const navigate = useNavigate();
+const handleBuy = () => {
+  navigate('/ico');
+};
 
   return (
     <div className={css.container}>
           <div>
-      {tokensPurchased === 0 ? (
-        whitelistAllocation !== 0 ? (
+      {userBalance === 0 || userBalance === null ? (
+        userBalance !== 0 ? (
           <div className={css.buytokensButton}>
             <h5>Congratulations! You are in the whitelist for the Libertum pre-sale. Don't miss this opportunity!</h5>
             <div className={css.buytokensbuttonSection}>
-              <p>You can buy up to {whitelistAllocation} tokens!</p>
-              <button>BUY TOKENS</button>
+              <p>You can buy up to {getUserWhitelistAllocation} tokens!</p>
+              <button onClick={handleBuy}>BUY TOKENS</button>
             </div>
           </div>
         ) : (
           <div className={css.buytokensButton}>
             <h5>Don't miss this opportunity! Buy tokens now.</h5>
-            <button>BUY TOKENS</button>
+            <button onClick={handleBuy}>BUY TOKENS</button>
           </div>
         )
       ) : (
         <div>        
           <div className={css.tokens}>
-            <h3>{tokensPurchased}</h3>
+            <h3>{userBalance}</h3>
             <h2>tokens purchased</h2>
           </div>
           <div className={css.buytokensButton}>
@@ -61,7 +84,7 @@ const Coin = () => {
         </div>
 
       )}
-    </div>
+          </div>
       
       <div className={css.table}>
         <h3>My Investments</h3>
