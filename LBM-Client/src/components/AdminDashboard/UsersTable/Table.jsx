4776211764a2e053fd023af66afe2be90a85194e
nikkiburgos <burgosveganicole@gmail.com>
2023-11-14 20:02:24 -0300

    users list - add to whitelist - fixed draft

--- LBM-Client/src/components/AdminDashboard/UsersTable/Table.jsx
+++ LBM-Client/src/components/AdminDashboard/UsersTable/Table.jsx
@@ -1,122 +1,90 @@
-import DataTable from 'react-data-table-component';   
+// En el componente Users
+import { ethers } from 'ethers';
+
+import pLBM_ABI from '../../../smartContracts/ABI/pLBM.json'
+import DataTable from 'react-data-table-component';
 import css from './TableUsers.module.scss';
 
+const pLBM_address = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+const provider = new ethers.providers.JsonRpcProvider();
+const signer = provider.getSigner();
+const pLBM_contract = new ethers.Contract(pLBM_address, pLBM_ABI.abi, signer);
+
 const customStyles = {
-    headRow: {
+  headRow: {
+    style: {
+      color: 'var(--0-c-0507, #0C0507)',
+      fontFamily: 'Inter',
+      fontSize: '0.875rem',
+      fontWeight: 700,
+      textTransform: 'capitalize',
+    },
+  },
+  conditionalCellStyles: [
+    {
+      when: (row) => row.selector === 'status',
       style: {
-        color: 'var(--0-c-0507, #0C0507)',
-        fontFamily: 'Inter',
-        fontSize: '0.875rem',
-        fontWeight: 700,
-        textTransform: 'capitalize'
-      }
+        borderRadius: '4px',
+        backgroundColor: 'blue',
+        color: 'white',
+      },
     },
-    conditionalCellStyles: [
-        {
-          when: row => row.selector === 'status',
-          style: {
-            borderRadius: '4px',
-            backgroundColor: 'blue', // Aquí puedes cambiar el color de fondo deseado
-            color: 'white', // Aquí puedes cambiar el color del texto deseado
-          }
-        }
-      ]
-  };
-  
-
-const columns = [
-    {
-        name: 'user',
-        selector: row => row.user,
-        
-        
-    },
-    {
-        name: 'Mail address',
-        selector: row => row.mail,
-    },
-    {
-        name: "",
-        selector: row => row.empty
-    },
-    {
-        name: "",
-        selector: row => row.empty
-    },
-    
-    {
-        name: 'View contracts',
-        selector: row => row.contract,
-        cell: row => (
-            <div
-              style={{
-                backgroundColor: 'gray', // Cambia el color de fondo deseado
-                borderRadius: '4px', // Añade el borde redondeado
-                color: 'white', // Cambia el color del texto deseado
-                padding: '8px', // Ajusta el espaciado interno según sea necesario
-
-              }}
-            >
-              {row.status}
-            </div>
-    
-           ) }]
-
-const data = [
-    {
-        id: 1,
-        user: 'Alan',
-        mail: "allena@gmail.com",
-        status: "contract"
-    },
-    {
-        id: 2,
-        user: 'Guillermo',
-        mail: "guillermo@gmail.com",
-        status: "contract"
-    },
-    {
-        id:3,
-        user: 'Maria',
-        mail: "guillermo@gmail.com",
-        status: "contract"
-    },
-    {
-        id: 4,
-        user: 'Pedro',
-        mail: "guillermo@gmail.com",
-        status: "contract"
-    },
-    {
-        id: 5,
-        user: 'Laura',
-        mail: "guillermo@gmail.com",
-        status: "contract"
-    },
-    {
-        id: 6,
-        user: 'Paula',
-        mail: "guillermo@gmail.com",
-        status: "contract"
-    },
-]
-
-const Users = () => {
-    return (
-        <div className={css.container}>
-        <div className={css.table}>
-        <h3></h3>
-        <DataTable
-            columns={columns}
-            data={data}
-            customStyles={customStyles}
-            
-        />
-        </div>
-        </div>
-
-
-    );
+  ],
 };
 
-export default Users; 
+
+const Users = ({ users }) => {
+  const data = users.map((user) => ({
+    id: user.ID_user,
+    user: user.editableName,
+    mail: user.email,
+    investedPropertiesAddress: user.investedProperties.Address,
+  }));
+
+    const addToWhitelist = async (userAddress) => {
+    const amount = 10;
+    await pLBM_contract.modifyWhitelistAllocations([userAddress], [BigInt(amount * 10 ** 18)]);
+  };
+
+  const columns = [
+    {
+      name: 'user',
+      selector: (row) => row.user,
+    },
+    {
+      name: 'Mail address',
+      selector: (row) => row.mail,
+    },
+    {
+      name: 'Add to whitelist',
+      cell: (row) => (
+        <button
+          onClick={() => addToWhitelist(row.investedPropertiesAddress)}
+          style={{
+            backgroundColor: 'gray',
+            borderRadius: '4px',
+            color: 'white',
+            padding: '8px',
+          }}
+        >
+          Add to whitelist
+        </button>
+      ),
+    },
+  ];
+
+  return (
+    <div className={css.container}>
+      <div className={css.table}>
+        <h3></h3>
+        <DataTable
+          columns={columns}
+          data={data}
+          customStyles={customStyles}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Users;
