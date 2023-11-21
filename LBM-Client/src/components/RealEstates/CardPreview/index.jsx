0b6ba4087db66f17d850ce39f6a3c97d9bee0fcb
nikkiburgos <burgosveganicole@gmail.com>
2023-11-20 22:32:08 -0300

    carrousel fixed

--- LBM-Client/src/components/RealEstates/CardPreview/index.jsx
+++ LBM-Client/src/components/RealEstates/CardPreview/index.jsx
@@ -18,9 +18,9 @@ const Index = (props) => {
     location,
     PIT,
     PRY,
-    tokenised,
-    NFTPrice,
-    AvailablesNFT,
+    // tokenised,
+    // NFTPrice,
+    // AvailablesNFT,
     price,
     capital,
     country
@@ -28,6 +28,7 @@ const Index = (props) => {
 
   // ---------------- IMAGES CARROUSEL -------------------------------
   const images = [image, image2, image3, image4, image5];
+  console.log('IMAGENES ' + images)
 
   const sliderSettings = {
     dots: true,
@@ -43,7 +44,6 @@ const Index = (props) => {
 
   const numericPrice = parseFloat(price);
   const formattedPrice = numericPrice.toLocaleString('en-US', { maximumFractionDigits: 0 });
-  console.log(formattedPrice)
 
   return (
 
@@ -51,27 +51,27 @@ const Index = (props) => {
 
       <div className="card-image-container">
 
-        <img src={image} alt="property image" />
-
-      <div className="card-image-container-savebutton">
-      <SaveProperty propertyId={props.id} />
-      </div>
-      
-      <div className="card-progress">
-        <progress max="100" value={fundedBar}></progress>
-        <div className="progress-text">{fundedBar}% Funded</div>
-      </div>
-
-      {/* <Slider {...sliderSettings}>
+        {/* <img src={image} alt="property image" /> */}
+        <Slider {...sliderSettings}>
           {images.map((img, index) => (
-            <div key={index}>
+            <div key={index} className="card-img">
               <Link to={`/marketplace/${id}`}>
                 <img src={img} alt={`Image ${index}`} />
               </Link>
             </div>
           ))}
-        </Slider> */}
+        </Slider>
+
+        <div className="card-image-container-savebutton">
+          <SaveProperty propertyId={props.id} />
+        </div>
+      
+        <div className="card-progress">
+          <progress max="100" value={fundedBar}></progress>
+          <div className="progress-text">{fundedBar}% Funded</div>
+        </div>
       </div>
+
       <div className="card-content">
         <div className="card-content-header">
           <div className="card-content-header-title">
@@ -82,13 +82,10 @@ const Index = (props) => {
             <p>Proyected rental yield {PRY} % </p>
             {capital > 0 && <p>Annual repayment: {annualRepayment} %</p>}
           </div>
-
-          {/* <SaveProperty propertyId={props.id} /> */}
         </div>
+       
         <div className="card-content-columns">
           <div className="card-content-details">
-            {/* <h5>1400  <span>tokens for sale</span></h5>
-            <h5>$50  <span>each token</span></h5> */}
             <p><span>1400 tokens for sale · $50 each token</span></p>
             <p>Rental Income per token: ${PIT} </p>
             {capital ? <p>Capital Repayment: {capitalRepayment} % </p> : null}            
