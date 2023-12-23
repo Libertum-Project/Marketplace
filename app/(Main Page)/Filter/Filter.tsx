762463a2536348ae257b86b6fcabbe2b8b055ed1
madhava <galarzaguillermo.ggm@gmail.com>
2023-12-23 17:15:54 -0300

    Feat (Filter): Filter UI for Desktop

--- app/(Main Page)/Filter/Filter.tsx
+++ app/(Main Page)/Filter/Filter.tsx
@@ -1,14 +1,14 @@
 import { type ReactElement } from "react";
 import css from "./Filter.module.css";
 import Image from "next/image";
-import searchImage from "./search.svg";
+import searchImage from "./whiteSearchBtn.svg";
 import moreOptionsImage from "./moreOptions.svg";
 
 export function Filter(): ReactElement {
   return (
     <section className={css.filterContainer}>
       <div className={css.searchByName}>
-        <input type="text" name="" id="" />
+        <input type="text" name="" id="" placeholder="Search by name" />
         <button className={css.moreOptionsBtn}>
           <Image src={moreOptionsImage} alt="Search" />
         </button>
@@ -16,16 +16,31 @@ export function Filter(): ReactElement {
       <div className={css.filterList}>
         <div>
           <label htmlFor="location">Location</label>
-          <select id="location"></select>
+          <select id="location">
+            <option value="">All</option>
+            <option value="passiveIncome">Passive Income</option>
+            <option value="capitalRepayment">Capital Repayment</option>
+          </select>
         </div>
         <div>
           <label htmlFor="rentalYield">Rental Yield</label>
-          <select id="rentalYield"></select>
+          <select id="rentalYield">
+            <option value="">All</option>
+            <option value="passiveIncome">Passive Income</option>
+            <option value="capitalRepayment">Capital Repayment</option>
+          </select>
         </div>
         <div>
           <label htmlFor="financeType">Finance Type</label>
-          <select id="financeType"></select>
+          <select id="financeType" value="all">
+            <option value="">All</option>
+            <option value="passiveIncome">Passive Income</option>
+            <option value="capitalRepayment">Capital Repayment</option>
+          </select>
         </div>
+        <button className={css.searchBtn}>
+          <Image src={searchImage} alt="Search" />
+        </button>
       </div>
     </section>
   );
