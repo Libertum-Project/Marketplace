42452258ae228204c13173170685bc42294b69a0
madhava <galarzaguillermo.ggm@gmail.com>
2024-01-08 15:06:06 -0300

    Feat(buy): Redirect User

--- app/(Routes)/property/buy/[propertyID]/page.tsx
+++ app/(Routes)/property/buy/[propertyID]/page.tsx
@@ -1,22 +1,27 @@
 "use client";
-import { type ReactElement } from "react";
+import { useRouter } from "next/navigation";
 import { useContext } from "react";
+import Loading from "@/app/components/MessageBox/Loading";
 import PropertyContext from "@/app/context/PropertyContext";
 
 export default function PropertyDetails({
   params,
 }: {
   params: { propertyID: string };
-}): ReactElement {
+}): any {
+  const router = useRouter();
+
   const { getPropertyDetails, isLoading }: any = useContext(PropertyContext);
   const ID: number = parseInt(params.propertyID);
 
   const propertyDetails = getPropertyDetails(ID);
-  console.log(propertyDetails);
 
-  if (isLoading) {
-    return <p>Loading property details...</p>;
+  if (!isLoading && !propertyDetails.length) {
+    router.push("../404");
+    return <Loading />;
   }
+
+  console.log(propertyDetails);
 
   return (
     <>
