049247862753531f5e2b4f9f24ac23386e1343f0
Abhishek Mishra <abhishek@Abhisheks-MacBook-Pro-2.local>
2024-03-06 18:24:20 +0530

    updated readme

--- components/Dashboard/Navbar/Navbar.tsx
+++ components/Dashboard/Navbar/Navbar.tsx
@@ -3,13 +3,16 @@ import Search from '@/components/shared/Search';
 import User from '@/components/shared/User';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="flex justify-between bg-white py-2 px-5 border-b border-black border-opacity-5 w-full items-center max-sm:bg-[#080915]">
       <div>
-        <p className="text-black text-opacity-80 text-lg font-bold font-space_grotesk max-sm:hidden">
-          My Account
+        <p className="text-black text-opacity-80 text-lg font-bold font-space_grotesk max-sm:hidden capitalize">
+          My {pathname.replace('/', '')}
         </p>
         <Link href="/" className="px-5 sm:hidden">
           <Image
