485b443e83b7609b66f6cae87937edc8abff58e1
nikiburgos <burgosveganicole@gmail.com>
2024-03-06 19:04:03 -0300

    Card listing

--- app/(Home)/(Main Page)/PropertyCard/ListCard/ListCard.tsx
+++ app/(Home)/(Main Page)/PropertyCard/ListCard/ListCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 import css from "./ListCard.module.css";
 import Link from 'next/link';
+import saveIcon from "../../../../../public/assets/saveHeart.svg"
 import hotel from '../../../../../public/assets/hotel.svg';
 import rentalYield from '../../../../../public/assets/rentalyield.svg';
 import location from '../../../../../public/assets/filter2.svg';
@@ -24,12 +25,8 @@ const ListPropertyComponent: React.FC<ListPropertyProps> = ({
       {properties.map((property) => (
         <div
           key={property.Address}
-          className={`${css.propertyList} ${
-            expandedCard === property.Address ? css.propertyExpanded : ''
-          }`}
-          onClick={() => handleCardClick(property.Address)}
+          className={css.propertyList}
         >
-          <div className={css.property__header}>
             <Image
               src={property.Feature.Link_Image[0]}
               alt={property.Address}
@@ -37,27 +34,30 @@ const ListPropertyComponent: React.FC<ListPropertyProps> = ({
               height={200}
               className={css.property__image}
             />
-<div className={css.property__info}>
-                <div className={css.property__info_Secondframe}>
-                  <div className={css.property__info_Firstframe}>
-                    <p className={css.property__info_address}>
-                      {property.Feature.Address}
-                    </p>
-                    <p className={css.property__info_price}>
-                      ${' '}
-                      {parseInt(
-                        property.Financial.Market_value_of_the_property.replace(
-                          /,/g,
-                          ''
-                        ),
-                        10
-                      )}
-                    </p>
+
+            <div className={css.property__info}>
+              <div className={css.property__info_FirstFrame}>
+                <p className={css.property__info_address}>
+                  {property.Feature.Address}
+                </p>
+                <p className={css.property__info_country}>
+                  {property.Feature.City}, {property.Feature.State},{' '} {property.Feature.Country}{' '}
+                </p>
+                <p className={css.property__info_price}>
+                  ${' '}{parseInt(property.Financial.Market_value_of_the_property.replace(/,/g,''),10)}
+                </p>
+              </div>
+
+              <div className={css.property__info_SecondFrame}>
+                <div className={css.property__info_SecondFrame_Info}>
+                  <div>
+                    <p><b>Capital Repayment: </b> {property.Financial.Capital_repayment}% </p>
+                    <p><b>Income per token: </b>${property.Financial.Income_per_token} </p>
                   </div>
-                  <p className={css.property__info_country}>
-                    {property.Feature.City}, {property.Feature.State},{' '}
-                    {property.Feature.Country}{' '}
-                  </p>
+                  <div>
+                    <p><b>Repayment Term: </b> {property.Financial.Capital_payment_duration} years </p>                    
+                    <p><b>Price per token: </b>${property.Financial.Token_Price}</p> 
+                  </div>   
                 </div>
                 <div className={css.property__info_features}>
                   <div className={css.property__info_features_feature}>
@@ -67,7 +67,7 @@ const ListPropertyComponent: React.FC<ListPropertyProps> = ({
                       width={14}
                       height={14}
                     />
-                    <p>{property.Feature.Type}</p>
+                    <p><b>Property: </b>{property.Feature.Type}</p>
                   </div>
                   <div className={css.property__info_features_feature}>
                     <Image
@@ -95,71 +95,20 @@ const ListPropertyComponent: React.FC<ListPropertyProps> = ({
                     </p>
                   </div>
                 </div>
-                {/* Additional information to be shown when expanded */}
-                {expandedCard === property.Address && (
-                  <table className={css.table}>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <strong>Token Price:</strong>
-                        </td>
-                        <td>${property.Financial.Token_Price}</td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Total Tokens:</strong>
-                        </td>
-                        <td>{property.Financial.Number_of_tokens_available}</td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Market Value:</strong>
-                        </td>
-                        <td>
-                          ${property.Financial.Market_value_of_the_property}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Income per Token:</strong>
-                        </td>
-                        <td>${property.Financial.Income_per_token}</td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Capital Repayment:</strong>
-                        </td>
-                        <td>{property.Financial.Capital_repayment}%</td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Repayment Term:</strong>
-                        </td>
-                        <td>
-                          {property.Financial.Capital_payment_duration} years
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Rental Yield:</strong>
-                        </td>
-                        <td>{property.Financial.Rental_yield}%</td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <strong>Annual Return:</strong>
-                        </td>
-                        <td>{property.Financial.Annual_return}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                )}
-                <Link href="/" className={css.property__info_button}>
-                  View Property
-                </Link>
               </div>
+
+              <div className={css.property__info_ThirdFrame}>
+                <Image
+                src={saveIcon}
+                alt={property.Address}
+                width={272}
+                height={200}
+                className={css.property__saveIcon}
+              />
+
+              </div>
+            </div>
           </div>
-        </div>        
       ))}
     </article>
   );
