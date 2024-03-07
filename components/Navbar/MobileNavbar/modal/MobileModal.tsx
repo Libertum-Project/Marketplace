7d4359da714083e2c4d82ccce4be21c6103bb77d
Abhishek Mishra <abhishek@Abhisheks-MacBook-Pro-2.local>
2024-03-07 09:52:07 +0530

    fixed deplo error

--- components/Navbar/MobileNavbar/modal/MobileModal.tsx
+++ components/Navbar/MobileNavbar/modal/MobileModal.tsx
@@ -1,7 +1,7 @@
-import { type ReactElement } from "react";
-import Link from "next/link";
-import css from "./MobileModal.module.css";
-import { ConnectWalletButton } from "../../WalletComponents/ConnectWalletButton";
+import { type ReactElement } from 'react';
+import Link from 'next/link';
+import css from './MobileModal.module.css';
+import ConnectWalletButton from '../../WalletComponents/ConnectWalletButton';
 
 interface MobileModalProp {
   handleToggleOpenMenu: () => void;
@@ -30,9 +30,15 @@ export function MobileModal({
         </Link>
         <div>
           <p>Docs</p>
-          <Link  href= "/Libertum_Tokenomics.pdf" target="_blank">→ Token Utility</Link>
-          <Link href="/whitepaperLibertum.pdf" target="_blank">→ White Paper</Link>
-          <Link href= "/Libertum_Pitch.pdf" target="_blank">→ Pitch Deck</Link>
+          <Link href="/Libertum_Tokenomics.pdf" target="_blank">
+            → Token Utility
+          </Link>
+          <Link href="/whitepaperLibertum.pdf" target="_blank">
+            → White Paper
+          </Link>
+          <Link href="/Libertum_Pitch.pdf" target="_blank">
+            → Pitch Deck
+          </Link>
         </div>
         <ConnectWalletButton />
       </div>
