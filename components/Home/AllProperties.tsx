e2ce263627c5375517146feabc3fdc75a7e86f86
Abhishek Mishra <abhishek@Abhisheks-MacBook-Pro-2.local>
2024-03-08 17:53:55 +0530

    added shared property card to the home page

---
+++ components/Home/AllProperties.tsx
@@ -0,0 +1,78 @@
+'use client';
+import { useState } from 'react';
+import PropertyCard from '../shared/PropertyCard';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+
+interface Props {
+  properties: {}[];
+}
+
+const AllProperties = ({ properties }: Props) => {
+  const [viewType, setViewType] = useState('grid');
+
+  const handleViewType = (type: string) => {
+    setViewType(type);
+  };
+
+  const propertyWrapperClassName =
+    viewType == 'grid'
+      ? 'py-5 grid grid-cols-1 min-[575px]:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-[1200px] m-auto'
+      : 'flex flex-col py-5 max-w-[1200px] m-auto gap-8';
+
+  return (
+    <div className="max-w-[1200px] m-auto">
+      <div className="flex justify-between items-center">
+        <select
+          defaultValue="Select"
+          className="min-w-[360px] px-3 py-2 bg-slate-900 bg-opacity-5 rounded-[5px] border border-black border-opacity-10 cursor-pointer"
+        >
+          <option value="Newest first">Sort by: Newest first</option>
+          <option value="Old First">Sort by: Old first</option>
+        </select>
+
+        <div className="flex items-center bg-neutral-100 rounded-[5px] gap-2 px-2 py-[5px]">
+          <Button className="p-0" onClick={() => handleViewType('grid')}>
+            <Image
+              src={`${
+                viewType == 'grid'
+                  ? '/assets/gridActive.svg'
+                  : '/assets/gridInactive.svg'
+              }`}
+              alt="Grid"
+              width="32"
+              height="32"
+            />
+          </Button>
+          <Button className="p-0" onClick={() => handleViewType('list')}>
+            <Image
+              src={`${
+                viewType == 'list'
+                  ? '/assets/filesActive.svg'
+                  : '/assets/filesInactive.svg'
+              }`}
+              alt="List"
+              width="32"
+              height="32"
+            />
+          </Button>
+        </div>
+      </div>
+      <div className={propertyWrapperClassName}>
+        {properties.map((property: any) => {
+          const address = `${property.address.city},${property.address.state},${property.address.country}`;
+          return (
+            <PropertyCard
+              key={property.id}
+              property={property}
+              address={address}
+              viewType={viewType}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AllProperties;
