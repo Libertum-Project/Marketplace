e4d3b081298f2eed4da7c55222ec2f5c4a52de76
Abhishek Mishra <abhishek@Abhisheks-MacBook-Pro-2.local>
2024-03-12 00:44:02 +0530

    fix homage banner

--- app/(Home)/(Main Page)/Hero/hero.tsx
+++ app/(Home)/(Main Page)/Hero/hero.tsx
@@ -1,67 +1,48 @@
-'use client'
-import { type ReactElement,  useState, useEffect } from "react";
-import Image from "next/image";
-import css from "./hero.module.css";
-import Link from "next/link";
-import photosMarket  from "../../../../public/assets/photosMarket.svg";
+import css from './hero.module.css';
+import Link from 'next/link';
+import { ServerImage } from '@/components/shared/ServerImage';
 
-export function Hero(): ReactElement {
-
-  const [isMobile, setIsMobile] = useState(false);
-  
-  useEffect(() => {
-  const mediaQuery = window.matchMedia("(max-width: 950px)");
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 950);
-      setIsMobile(mediaQuery.matches);
-    };
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", handleResize);
-      handleResize();
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
-
+const Hero = () => {
   return (
     <div className={css.heroContainer}>
-      {isMobile ? 
-        <div className={css.backgroundNoVideo}></div> 
-        : 
+      <div className="max-sm:hidden">
         <video autoPlay muted loop className={css.video}>
           <source src="./bg-video-1.mp4" type="video/mp4" />
-        </video> 
-      }
-      <div className={css.heroCommunity}>
+        </video>
+      </div>
+
+      <div className={`${css.heroCommunity} max-sm:bg-primary-gradient`}>
         <div className={css.textCommunity}>
           <h1>Unlock the potential for earning by investing</h1>
           <div className={css.paragraphsCommunity}>
             <p>
-            From residential havens to commercial hubs, each property presents unique opportunities for growth and prosperity. 
-            </p>          
+              From residential havens to commercial hubs, each property presents
+              unique opportunities for growth and prosperity.
+            </p>
           </div>
         </div>
 
-        <div className={css.featuredProperties}>
-        <Image
-                src={photosMarket}
-                alt="N"
-                width="336"
-                height="206"
-                className={css.featuredProperties__image}
+        <div className="flex items-center">
+          <ServerImage
+            src="/assets/property.png"
+            alt="N"
+            width="336"
+            height="206"
           />
-          <div className={css.featuredProperties__box}>
+          <div className="pl-10 pr-4 py-4 bg-white bg-opacity-5 rounded-xl border border-l-0 rounded-l-none border-teal-500">
             <div className={css.featuredProperties__title}>
-            <span>New</span>
-            <p>Featured Properties</p>
+              <span>New</span>
+              <p>Featured Properties</p>
             </div>
-            
-            <Link href="/" className={css.featuredProperties__button}>See all featured</Link>
+
+            <Link href="/" className={css.featuredProperties__button}>
+              See all featured
+            </Link>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Hero;
--- app/(Home)/(Main Page)/page.tsx
+++ app/(Home)/(Main Page)/page.tsx
@@ -1,17 +1,16 @@
-import { Hero } from './Hero/hero';
+import Hero from './Hero/hero';
 import { Filters } from './Filters/Filters';
 import { properties } from '@/constants';
 import AllProperties from '@/components/Home/AllProperties';
 
 const Home = () => {
   return (
-    <div >
+    <div>
       <Hero />
-      <div className='md:max-w-[75rem] m-auto'> 
-      <Filters />
-      <AllProperties properties={properties} />
+      <div className="md:max-w-[75rem] m-auto">
+        <Filters />
+        <AllProperties properties={properties} />
       </div>
-
     </div>
   );
 };
