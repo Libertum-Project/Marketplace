ecd40b1c333118bcc77b89a02ec10aa3d59d09df
nikiburgos <burgosveganicole@gmail.com>
2024-03-21 17:22:08 -0300

    fixes detail

--- components/PropertyDetail/Images.tsx
+++ components/PropertyDetail/Images.tsx
@@ -9,14 +9,14 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from '@/components/ui/dialog';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carrousel"
+} from '@/components/ui/carrousel';
 
 interface ImageGalleryProps {
   images: string[];
@@ -27,38 +27,38 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
 
   return (
     <div className="max-w-screen-lg mx-auto space-y-2">
-        <div className="relative max-w-screen-lg">
-          <ServerImage 
-            src={firstFourImages[0]} 
-            alt="Main Image" 
-            width={509}
-            height={339}
-            className="w-full h-auto md:col-span-2 rounded-[5px]" 
-          />
+      <div className="relative max-w-screen-lg">
+        <ServerImage
+          src={firstFourImages[0]}
+          alt="Main Image"
+          width={509}
+          height={339}
+          className="w-full h-auto md:col-span-2 rounded-[5px]"
+        />
 
-          <Dialog>
-            <DialogTrigger>
-              <div className="absolute bottom-9 right-4 flex items-center gap-2 z-30 bg-white rounded-[56px] p-2 backdrop-blur-[5px]">
-                <ServerImage 
-                  src="/assets/icons/gallery.svg"
-                  alt="gallery"
-                  width={16}
-                  height={16}         
-                  />
-                  <p>View photo gallery</p>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
+        <Dialog>
+          <DialogTrigger>
+            <div className="absolute bottom-9 right-4 flex items-center gap-2 z-30 bg-white rounded-[56px] p-2 backdrop-blur-[5px]">
+              <ServerImage
+                src="/assets/icons/gallery.svg"
+                alt="gallery"
+                width={16}
+                height={16}
+              />
+              <p>View photo gallery</p>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="p-0">
             <Carousel>
-            <CarouselContent>
+              <CarouselContent>
                 {images.map((image, index) => (
                   <CarouselItem key={index}>
-                    <ServerImage 
-                      src={image} 
-                      alt={`Image ${index}`} 
+                    <ServerImage
+                      src={image}
+                      alt={`Image ${index}`}
                       width={509}
                       height={339}
-                      className="w-full h-auto" 
+                      className="w-full h-auto"
                     />
                   </CarouselItem>
                 ))}
@@ -66,24 +66,22 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
               <CarouselPrevious />
               <CarouselNext />
             </Carousel>
-            </DialogContent>
-          </Dialog>
+          </DialogContent>
+        </Dialog>
+      </div>
 
-        </div>
-
-
-        <div className="grid grid-cols-3 gap-4 md:col-span-2">
-          {firstFourImages.slice(1).map((image, index) => (
-            <ServerImage 
-              key={index} 
-              src={image} 
-              alt={`Image ${index}`}
-              width={163}
-              height={109}
-              className="w-full h-auto rounded-[5px]" 
-            />
-          ))}
-        </div>
+      <div className="grid grid-cols-3 gap-4 md:col-span-2">
+        {firstFourImages.slice(1).map((image, index) => (
+          <ServerImage
+            key={index}
+            src={image}
+            alt={`Image ${index}`}
+            width={163}
+            height={109}
+            className="w-full h-auto rounded-[5px]"
+          />
+        ))}
+      </div>
     </div>
   );
 };
