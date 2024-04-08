815ab2bdbe95d9e079491ffabd80b45e0a465eac
nikiburgos <burgosveganicole@gmail.com>
2024-04-08 14:05:22 -0300

    earn page

--- app/(Dashboard)/lbm-coin/page.tsx
+++ app/(Dashboard)/lbm-coin/page.tsx
@@ -4,20 +4,23 @@ import { ServerImage } from '@/components/shared/ServerImage';
 
 const page = () => {
   return (
-    <>
+    <div>
+      <div className="flex w-full p-5 justify-center items-center bg-libertumOrange shadow-sm">
+        <p className="text-white ">
+          This page is under construction. Thanks for your patience
+        </p>
+      </div>
       <div className={css.container}>
         <div className={css.background}></div>
         <div className={css.frame}>
-        <h2>Coming Soon: </h2>
-          <ServerImage 
-          src="/assets/earnDARK.svg"
-          alt='earn image logo'
-          width={400}
-          height={200}
+          <h2>Coming Soon: </h2>
+          <ServerImage
+            src="/assets/earnDARK.svg"
+            alt="earn image logo"
+            width={400}
+            height={200}
           />
-          <p>
-            Token analytics and staking tool
-          </p>
+          <p>Token analytics and staking tool</p>
 
           <Link href="/">
             <div className={css.button}>
@@ -26,7 +29,7 @@ const page = () => {
           </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
