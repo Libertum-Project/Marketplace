ebffff8f0dbb4bc71597c5be75c9b3ff14224618
madhava <galarzaguillermo.ggm@gmail.com>
2024-04-24 15:23:31 -0300

    Conditionally display the demo property

--- components/Dashboard/Investments/Investments.tsx
+++ components/Dashboard/Investments/Investments.tsx
@@ -1,18 +1,28 @@
 'use client';
+import { getProperties } from '@/app/utils/fetchProperties';
 import { useAddress } from '@thirdweb-dev/react';
 import { useState, useEffect } from 'react';
 import PropertyCard from '@/components/shared/PropertyCard';
 import { CardContent } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Card } from '@/components/ui/card';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
 
 const Investments = () => {
   const userWalletAddress: string | undefined = useAddress();
   const [properties, setProperties] = useState([]);
+  const [demoProperties, setDemoProperties] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchProperties() {
+      const demoProperties = await getProperties();
+      setDemoProperties(demoProperties)
       const response = await fetch(
         `/api/users/investments?userWalletAddress=${userWalletAddress}`,
         {
@@ -34,43 +44,69 @@ const Investments = () => {
   return (
     <>
       <div className="p-5 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 w-full sm:investment-page-width justify-start items-start">
-        {loading
-          ? // Show skeleton cards when loading
-            [...Array(3)].map((_, index) => (
-              <Card key={`skeleton-${index}`}>
-                <CardContent className="h-480 p-0">
-                  <div className="flex flex-col space-y-3 w-full">
-                    <Skeleton className="h-[255px] bg-[#929191] w-full" />
-                    <div className="space-y-4 p-4">
-                      <Skeleton className="h-10 w-full bg-black bg-opacity-5 rounded-[48px]" />
+        {loading ? (
+          // Show skeleton cards when loading
+          [...Array(3)].map((_, index) => (
+            <Card key={`skeleton-${index}`}>
+              <CardContent className="h-480 p-0">
+                <div className="flex flex-col space-y-3 w-full">
+                  <Skeleton className="h-[255px] bg-[#929191] w-full" />
+                  <div className="space-y-4 p-4">
+                    <Skeleton className="h-10 w-full bg-black bg-opacity-5 rounded-[48px]" />
 
-                      <div className="flex items-center justify-between">
-                        <div className="pr-4 w-full">
-                          <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
-                        </div>
-                        <div className="w-full">
-                          <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
-                        </div>
-                        <div className="pl-4 w-full">
-                          <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
-                        </div>
+                    <div className="flex items-center justify-between">
+                      <div className="pr-4 w-full">
+                        <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
                       </div>
-
-                      <Skeleton className="h-10 w-full bg-black bg-opacity-5 rounded-[48px]" />
+                      <div className="w-full">
+                        <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
+                      </div>
+                      <div className="pl-4 w-full">
+                        <Skeleton className="h-7 w-full bg-black bg-opacity-5 rounded-[48px]" />
+                      </div>
                     </div>
+
+                    <Skeleton className="h-10 w-full bg-black bg-opacity-5 rounded-[48px]" />
                   </div>
-                </CardContent>
-              </Card>
-            ))
-          : // Render property cards when properties are available
-            properties.map((property: any) => (
-              <PropertyCard
-                property={property}
-                key={property.id}
-                btnTitle="View Investment Details"
-                investmentDetail={true}
-              />
-            ))}
+                </div>
+              </CardContent>
+            </Card>
+          ))
+        ) : // Render property cards when properties are available
+        properties.length > 0 ? (
+          properties.map((property: any) => (
+            <PropertyCard
+              property={property}
+              key={property.id}
+              btnTitle="View Investment Details"
+              investmentDetail={true}
+            />
+          ))
+        ) : (
+          // Show test property when user does not have any properties
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {demoProperties.slice(0, 1).map((property: any) => (
+                    <PropertyCard
+                      property={property}
+                      key={property.id}
+                      btnTitle="View Investment Details"
+                      investmentDetail={true}
+                    />
+                  ))}
+                </TooltipTrigger>
+                <TooltipContent
+                  side="bottom"
+                  className="bg-libertumOrange rounded-[5px] "
+                >
+                  <p className="text-white font-space_grotesk font-semibold text-sm py-2">
+                    This is an example of how you will see your investments.
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+        )}
       </div>
     </>
   );
