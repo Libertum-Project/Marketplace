efcad91d8624061b24a129adcd22de1f7fbeded1
madhava <galarzaguillermo.ggm@gmail.com>
2024-05-09 12:55:25 -0300

    Refactor Documents component in PropertyDetail.

--- components/PropertyDetail/Documents.tsx
+++ components/PropertyDetail/Documents.tsx
@@ -4,26 +4,24 @@ import downloadIcon from '../../public/assets/icons/download.svg';
 import Link from 'next/link';
 
 const Documents: React.FC<{ documents: string[] }> = ({ documents }) => {
+  if (documents.length === 0) {
+    return <p className="text-[#2196F3]">Documents not provided</p>;
+  }
+
   return (
     <div>
       {documents.map((document, index) => (
-        <div key={index} className="flex items-center gap-4">
-          {documents.length > 0 && documents[0] === 'no document provided' ? (
-            <p className="text-[#2196F3]">Documents not provided</p>
-          ) : (
-            <div key={index} className="flex items-center gap-4">
-              <p className="text-[#2196F3]">Document {index + 1}</p>
-              <Link href={document} passHref target="_blank">
-                <ServerImage
-                  src={downloadIcon}
-                  alt="Download Icon"
-                  width={20}
-                  height={20}
-                />
-              </Link>
-            </div>
-          )}
-        </div>
+        <Link href={document} key={index} passHref target="_blank">
+          <div key={index} className="flex items-center gap-4">
+            <p className="text-[#2196F3]">Document {index + 1}</p>
+            <ServerImage
+              src={downloadIcon}
+              alt="Download Icon"
+              width={20}
+              height={20}
+            />
+          </div>
+        </Link>
       ))}
     </div>
   );
