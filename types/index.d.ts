faee19a878c8c4a13b6c70ffa2e628701426b704
madhava <galarzaguillermo.ggm@gmail.com>
2024-05-21 13:27:23 -0300

    Chore: replace 'any' with 'Property' type in Investments component.
    - Created Property type in types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,31 @@ export interface Token {
   decimals: number;
   logo: string;
 }
+
+export interface Property {
+  amenities: string[];
+  annual_yield: number;
+  category: string;
+  contract_address: string;
+  createdAt: string;
+  description: string[];
+  documents: string[];
+  highlight_image: string;
+  id: number;
+  image_gallery: string[];
+  investedBy: null | string; 
+  listing_duration_months: number;
+  location: {
+    country: string;
+    region: string;
+    city: string;
+    address: string;
+    postal_code: string;
+  };
+  owner_id: number;
+  savedBy: number;
+  token_duration_months: number;
+  total_shares: number;
+  total_valuation: number;
+}
+
